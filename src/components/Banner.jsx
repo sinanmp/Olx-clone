eaac754536ab10d9e7216266f005e2237820c890
Muhammed Sinan Mp <sinanmhd5817@gmail.com>
2024-04-02 15:45:48 +0530

    priyan code

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Banner() {
   return (
-    <div className='w-full absolute left-0 top-[64px]'>
+    <div>
       <img
-        className='object-cover w-full  sm:h-[300px] md:h-[400px] lg:h-[400px] h-[200px] xl:h-[500px]'
+        className='min-w-min-[fit-content]'
         src="https://scontent-bom2-1.xx.fbcdn.net/v/t39.30808-6/420474973_781834080655372_6150883909337339173_n.png?stp=dst-jpg&_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_ohc=9OPYkgSfBCkAX86TpEM&_nc_ht=scontent-bom2-1.xx&oh=00_AfAjD-wcjMVA4o7vsBqb3dote6cj0eh-KLWA2H-rijKptQ&oe=66116C51"
         alt="Banner"
       />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,38 +11,33 @@ function navbar() {
     setSearch(e.target.value)
   }
    return (
-    <nav className='py-6 bg-slate-300 h-16 fixed w-full top-0 left-0 z-30 transition-colors duration-300'>
-        <div className='flex relative mainFlex  bottom-4 gap-5 left-3'>
-          <Link to={'/'}>
-         <svg className='' width="48px" height="48px" viewBox="0 0 1024 1024" data-aut-id="icon"><path  d="M661.333 256v512h-128v-512h128zM277.333 298.667c117.824 0 213.333 95.531 213.333 213.333s-95.509 213.333-213.333 213.333c-117.824 0-213.333-95.531-213.333-213.333s95.509-213.333 213.333-213.333zM794.496 384l37.504 37.504 37.504-37.504h90.496v90.496l-37.504 37.504 37.504 37.504v90.496h-90.496l-37.504-37.504-37.504 37.504h-90.496v-90.496l37.504-37.504-37.504-37.504v-90.496h90.496zM277.333 426.667c-47.061 0-85.333 38.293-85.333 85.333s38.272 85.333 85.333 85.333c47.061 0 85.333-38.293 85.333-85.333s-38.272-85.333-85.333-85.333z"></path></svg>
-          </Link>
-        
-         <div className='hidden sm:flex items-center'>
-        <FaSearch size={'15px'} opacity={4} className="ml-2 absolute" />
-        <input className='h-12 w-64 pl-8 border-2 rounded value border-black text-start' placeholder="" value="India" fdprocessedid="0b4aec"/>
-        </div>
-
-        <div className=' hidden h-auto md:flex'>
-       <input className=' md:w-[50vw] h-12 border-2 rounded  border-black text-start' onChange={handleSearch} type="text" placeholder="Find Cars, Mobile Phones and more..." data-aut-id="searchBox" value={search} />
-       <div className="bg-black p-2 rounded-r relative right-2">
-         <FaSearch className='relative top-1.5' size={20} color="white" />
-        </div>
-       </div> 
-
-        <div className='pt-3 flex gap-1'>
-            <p className='font-semibold'>ENGLISH</p>
-            <IoChevronDownOutline className=' pb-1.5 size-8' size={25} color="black" />
-        </div>
-        
-        
-        <div className=' md:pl-5 loginDiv max-[400]:'>
-        <p onClick={()=> navigate('/login')} className='font-semibold cursor-pointer text-lg pt-2 underline'>Login</p>
-        </div>
-       
-        <div className='flex pt-1  sellicon'>
-            <button className='border sellbutton rounded-3xl pl-2 w-20 md:w-23 border-black font-semibold relative left- h-10'><FaPlus className='absolute mt-[5px] pr-1 md:right-11 md:ml-5' size={15}/> SELL</button>
-        </div>
-
+    <nav className='min-w-[fit-content]'>
+        <div className='flex items-center gap-4 py-3 px-4 bg-[rgba(0, 47, 52, 0.03)] bg-slate-300'>
+          <div>
+            <svg onClick={()=>navigate('/')} className='md:w-12 md:h-12 w-20 h-20 cursor-pointer' width="48px" height="48px" viewBox="0 0 1024 1024" data-aut-id="icon" ><path  d="M661.333 256v512h-128v-512h128zM277.333 298.667c117.824 0 213.333 95.531 213.333 213.333s-95.509 213.333-213.333 213.333c-117.824 0-213.333-95.531-213.333-213.333s95.509-213.333 213.333-213.333zM794.496 384l37.504 37.504 37.504-37.504h90.496v90.496l-37.504 37.504 37.504 37.504v90.496h-90.496l-37.504-37.504-37.504 37.504h-90.496v-90.496l37.504-37.504-37.504-37.504v-90.496h90.496zM277.333 426.667c-47.061 0-85.333 38.293-85.333 85.333s38.272 85.333 85.333 85.333c47.061 0 85.333-38.293 85.333-85.333s-38.272-85.333-85.333-85.333z"></path></svg>
+          </div>
+          <div className='relative w-auto h-auto hidden lg:flex'>
+            <FaSearch size={'15px'} opacity={4} className="ml-2 absolute top-[50%] translate-y-[-50%]" />
+            <input className='md:h-12 md:w-64 h-20 w-72 px-8 border-2 rounded value border-black text-start' placeholder="" value="India" fdprocessedid="0b4aec"/>
+          </div>
+          <div className='flex'>
+            <input className='w-[30vw] md:w-[45vw] h-12 border-2 rounded-l-[4px] border-black text-start px-2' onChange={handleSearch} type="text" placeholder="Find Cars, Mobile Phones and more..." data-aut-id="searchBox" value={search} />
+            <button className='bg-[#002f34] w-12 h-12 flex justify-center items-center rounded-r-[4px]'>
+              <FaSearch size={24} color="white" />
+            </button>
+          </div>
+          <div className='flex items-center'>
+            <p className='font-[700] text-[18px] md:text-[14px]'>ENGLISH</p>
+            <IoChevronDownOutline className='size-8 font-normal' size={14} color="black" />
+          </div>
+          <div className='flex justify-end items-center gap-5 w-[100%]'>
+            <div>
+              <p onClick={()=> navigate('/login')} className='font-semibold cursor-pointer text-xl md:text-lg underline'>Login</p>
+            </div>
+            <div className=''>
+              <button className='sellbutton rounded-3xl px-2 border-black font-semibold h-12 w-24 flex items-center gap-1'><FaPlus className='relative left-1' size={15}/> SELL</button>
+            </div>
+          </div>
         </div>
     </nav>
   )
